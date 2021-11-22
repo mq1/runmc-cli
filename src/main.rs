97bb9afa3feb18d7da31e682f9f4e1a70d1dc310
Manuel Quarneti <manuelquarneti@gmail.com>
2021-11-22 12:04:41 +0100

    add list-instances command

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,14 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     if matches.is_present("list-minecraft-versions") {
         let versions = libmc::launchermeta::get_minecraft_versions()?;
-        versions.into_iter().for_each(|version| println!("{} {}", version.r#type, version.id));
+        versions.iter().for_each(|version| println!("{} {}", version.r#type, version.id));
+        return Ok(())
+    }
+
+    if matches.is_present("list-instances") {
+        let instances = libmc::instances::get_instance_list()?;
+        instances.iter().for_each(|instance| println!("{}", instance));
+        return Ok(())
     }
 
     Ok(())
